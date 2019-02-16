79c09f23b5c8c678b0296a819755ed09295a2d68
Mason <SOROM2@github.com>
2019-02-17 03:29:21 +1300

    Made install script more verbose with a "done" message, changed /home/USER vars to HOME instead

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Your current rc files in /home/$USER will be deleted and replaced."
+echo "Your current rc files in $HOME will be deleted and replaced."
 printf "Replace .bashrc and .vimrc with the repo versions? (y/N): "
 
 read choice
@@ -8,7 +8,8 @@ choice=$( echo "$choice" | tr '[:upper:]' '[:lower:]' )
 
 if [ $choice == 'y' ] || [ $choice == 'yes' ] 
 then
-    ln -sf $PWD/bashrc /home/$USER/.bashrc
-    ln -sf $PWD/vimrc /home/$USER/.vimrc
+    ln -sf $PWD/bashrc $HOME/.bashrc
+    ln -sf $PWD/vimrc $HOME/.vimrc
+    echo "rc files in $HOME have been replaced with symlinks to the rc files in the git repo."
 fi
 
