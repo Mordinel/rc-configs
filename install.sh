aff63ffd508f8021885033e247ef16a402e96064
Mason <SOROM2@student.op.ac.nz>
2022-08-09 18:42:14 +1200

    Move to directory linking

--- install.sh
+++ install.sh
@@ -5,26 +5,23 @@ read -p "Replace with repo versions? (y/N): " choice
 
 if [[ "$choice" =~ ^[yY] ]]
 then
+    # home files
     ln -sf $PWD/bashrc $HOME/.bashrc
+    ln -sf $PWD/gtkrc-2.0 $HOME/.gtkrc-2.0
 
+    # home directories
     mkdir -p $HOME/.vim
     ln -sf $PWD/vimrc $HOME/.vim/vimrc
 
-    mkdir -p $HOME/.config/tmux
-    ln -sf $PWD/tmux.conf $HOME/.config/tmux/tmux.conf
-
-    mkdir -p $HOME/.config/alacritty
-    ln -sf $PWD/alacritty.yml $HOME/.config/alacritty/alacritty.yml
-
-    mkdir -p $HOME/.config/gtk-3.0/
-    ln -sf $PWD/gtk3.ini $HOME/.config/gtk-3.0/settings.ini
-
+    # ~/.config
+    ln -sf $PWD/tmux $HOME/.config/tmux
+    ln -sf $PWD/alacritty $HOME/.config/alacritty
+    ln -sf $PWD/gtk-3.0 $HOME/.config/gtk-3.0
     ln -sf $PWD/herbstluftwm $HOME/.config/herbstluftwm
     ln -sf $PWD/polybar $HOME/.config/polybar
     ln -sf $PWD/rofi $HOME/.config/rofi
     ln -sf $PWD/picom $HOME/.config/picom
 
-    ln -sf $PWD/gtkrc-2.0 $HOME/.gtkrc-2.0
     echo "Config files have been replaced with symlinks to the config files in this git repo."
 fi
 
