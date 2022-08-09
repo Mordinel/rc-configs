876919804e53fa1af01961e1d10926434730572d
Mason <SOROM2@student.op.ac.nz>
2022-08-09 18:49:23 +1200

    Update console only script

--- console_only_install.sh
+++ console_only_install.sh
@@ -5,13 +5,15 @@ read -p "Replace with repo versions? (y/N): " choice
 
 if [[ "$choice" =~ ^[yY] ]]
 then
+    # home files
     ln -sf $PWD/bashrc $HOME/.bashrc
 
+    # home directories
     mkdir -p $HOME/.vim
     ln -sf $PWD/vimrc $HOME/.vim/vimrc
 
-    mkdir -p $HOME/.config/tmux
-    ln -sf $PWD/tmux.conf $HOME/.config/tmux/tmux.conf
+    # ~/.config
+    ln -sf $PWD/tmux $HOME/.config/tmux
 
     echo "Config files have been replaced with symlinks to the config files in this git repo."
 fi
