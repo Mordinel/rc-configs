041fa67f62ae96cbdd2004999a37e62eac1f9e63
Mason <SOROM2@student.op.ac.nz>
2023-02-24 09:01:06 +1300

    remove default languages

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,10 +2,7 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-    "clangd",
     "rust_analyzer",
-    "pylsp",
-    "bashls",
 })
 
 local cmp = require("cmp")
