09fa5f1a6baf7cb6149b41c72237917e3075c6b9
Mordinel <mordinel@pm.me>
2023-10-13 11:49:14 +1300

    remove help from treesitter

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" 
-    ensure_installed = { "lua", "vim", "help" },
+    ensure_installed = { "lua", "vim" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
