50f2a2a453f371ddfc6020c630925942de6645e6
Mason <Mordinel@pm.me>
2024-02-07 20:42:37 +1300

    add a bind for copilot-suggest

--- nvim/after/plugin/copilot.lua
+++ nvim/after/plugin/copilot.lua
@@ -6,5 +6,6 @@ vim.keymap.set('i', '<C-Return>', 'copilot#Accept("\\<CR>")', opts)
 vim.keymap.set('i', '<C-J>', '<Plug>(copilot-next)')
 vim.keymap.set('i', '<C-K>', '<Plug>(copilot-previous)')
 vim.keymap.set('i', '<C-L>', '<Plug>(copilot-accept-word)')
+vim.keymap.set('i', '<C-\\>', '<Plug>(copilot-suggest)')
 vim.keymap.set('i', '<C-Backspace>', '<Plug>(copilot-dismiss)')
 
