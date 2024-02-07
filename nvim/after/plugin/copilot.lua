416860b6ad6839a7cb364b67f7643478f83263f6
Mason <Mordinel@pm.me>
2024-02-07 19:52:51 +1300

    Change copilot bindings, disable by default

--- nvim/after/plugin/copilot.lua
+++ nvim/after/plugin/copilot.lua
@@ -1,9 +1,10 @@
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_enabled = false
+
 local opts = {expr = true, replace_keycodes = false}
-vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', opts)
-vim.keymap.set('i', '<C-N>', '<Plug>(copilot-next)')
-vim.keymap.set('i', '<C-P>', '<Plug>(copilot-previous)')
+vim.keymap.set('i', '<C-Return>', 'copilot#Accept("\\<CR>")', opts)
+vim.keymap.set('i', '<C-J>', '<Plug>(copilot-next)')
+vim.keymap.set('i', '<C-K>', '<Plug>(copilot-previous)')
 vim.keymap.set('i', '<C-L>', '<Plug>(copilot-accept-word)')
 vim.keymap.set('i', '<C-Backspace>', '<Plug>(copilot-dismiss)')
-vim.g.copilot_no_tab_map = true
-vim.g.copilot_enabled = true
 
