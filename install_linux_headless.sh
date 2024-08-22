1cfc9f1c0b73a5ebcf56209949ba5e545d22589f
Mason <Mordinel@pm.me>
2024-08-22 12:31:30 +1200

    Make install scripts be more OS and situationally dependent

---
+++ install_linux_headless.sh
@@ -0,0 +1,38 @@
+
+#!/bin/bash
+
+if [[ -e "$PWD/util.sh" ]]; then
+    source "$PWD/util.sh"
+else
+    echo "Util script does not exist!"
+    exit 1
+fi
+
+echo "Your current config files will be deleted and replaced."
+read -p "Replace with repo versions? (y/N): " choice
+
+if [[ "$choice" =~ ^[yY] ]]
+then
+    # home files
+    Install $PWD/bashrc $HOME/.bashrc
+    Install $PWD/zshrc $HOME/.zshrc
+
+    # home directories
+    mkdir -p $HOME/.vim
+    Install $PWD/vimrc $HOME/.vim/vimrc
+    mkdir -p $HOME/.scripts
+    Install $PWD/scripts $HOME/.scripts
+
+    # ~/.config
+    Install $PWD/tmux $HOME/.config/tmux
+    Install $PWD/nvim $HOME/.config/nvim
+
+fi
+
+read -p "Run post-install scripts? (y/N): " choice
+if [[ "$choice" =~ ^[yY] ]]
+then
+    # install packer for neovim
+    [[ -e "$PWD/nvim/packer_install.sh" ]] && $PWD/nvim/packer_install.sh
+fi
+
--- install_mac.sh
+++ install_mac.sh
@@ -24,6 +24,7 @@ then
 
     # ~/.config
     Install $PWD/tmux $HOME/.config/tmux
+    Install $PWD/alacritty $HOME/.config/alacritty
     Install $PWD/nvim $HOME/.config/nvim
 fi
 
