bf31dc68a95d30653e4a12183678b6ccb915c8b1
Mason <Mordinel@pm.me>
2025-03-21 05:30:58 +1300

    Update from lsp zero v1.x

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,20 +1,42 @@
-local lsp = require("lsp-zero")
-lsp.preset("recommended")
+vim.opt.signcolumn = 'yes'
 
-local cmp = require("cmp")
-local cmp_mappings = lsp.defaults.cmp_mappings({
-    ["<C-Space>"] = cmp.mapping.complete(),
+-- Floating window borders
+vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+    vim.lsp.handlers.hover,
+    { border = 'rounded' }
+)
+vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+    vim.lsp.handlers.signature_help,
+    { border = 'rounded' }
+)
+
+-- Errors & Warnings
+vim.diagnostic.config({
+    virtual_text = true,
+    severity_sort = true,
+    float = {
+        style  = 'minimal',
+        border = 'rounded',
+        header = '',
+        prefix = '',
+    },
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = '✘',
+            [vim.diagnostic.severity.WARN]  = '▲',
+            [vim.diagnostic.severity.HINT]  = '⚑',
+            [vim.diagnostic.severity.INFO]  = '»',
+        },
+    },
 })
 
-lsp.setup_nvim_cmp({
-    mapping = cmp_mappings
-})
-
-lsp.on_attach(function()
-    local opts = {buffer = bufnr, remap = false}
-    vim.diagnostic.config({ virtual_text = true })
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-end)
+-- Add cmp_nvim_lsp capabilities settings to lspconfig
+local lspconfig_defaults = require('lspconfig').util.default_config
+lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+    'force',
+    lspconfig_defaults.capabilities,
+    require('cmp_nvim_lsp').default_capabilities()
+)
 
 vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('UserLspConfig', {}),
@@ -35,42 +57,172 @@ vim.api.nvim_create_autocmd('LspAttach', {
         -- docs
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
 
+        -- code actions
         vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
         vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', '<F3>', vim.lsp.buf.format({ async = true }))
     end,
 })
 
--- rust-analyzer
---require('lspconfig').rust_analyzer.setup {
---    cmd = { "rust-analyzer" },
---}
+require('mason').setup({})
+require('mason-lspconfig').setup({
+    handlers = {
+        -- Activate default handler for every language server without a custom handler.
+        function(server_name)
+            require('lspconfig')[server_name].setup({})
+        end,
 
--- Pylsp
---lsp.configure('pylsp', {
---    settings = {
---        pylsp = {
---            plugins = {
---                pycodestyle = {enabled = false},
---                pyflakes = {enabled = false},
---            },
---        },
---    },
---})
+        rust_analyzer = function()
+            require('lspconfig').rust_analyzer.setup({
+                cmd = { 'rust-analyzer' },
+                completion = { autoimport = { exclude = {
+                    { path = 'std::usize', type = "always" },
+                    { path = 'std::isize', type = "always" },
+                }}},
+            })
+        end,
 
--- OCaml
---require('lspconfig').ocamllsp.setup{}
+        pylsp = function()
+            require('lspconfig').pylsp.setup({
+                settings = {
+                    pylsp = {
+                        plugins = {
+                            pycodestyle = {enabled = false},
+                            pyflakes = {enabled = false},
+                        },
+                    },
+                },
+            })
+        end,
 
--- Omnisharp Extended
---local omnisharp_ext = require("omnisharp_extended")
---lsp.configure('omnisharp', {
---    handlers = {
---        ["textDocument/definition"] = omnisharp_ext.handler,
---    }
---})
+        lua_ls = function()
+            require('lspconfig').lua_ls.setup({
+                settings = {
+                    Lua = {
+                        runtime = {
+                            'LuaJIT',
+                        },
+                        diagnostics = {
+                            globals = { 'vim' },
+                        },
+                        workspace = {
+                            library = { vim.env.VIMRUNTIME },
+                        },
+                    },
+                },
+            })
+        end,
 
--- Sourcekit
---require('lspconfig').sourcekit.setup {
---  cmd = {'/Library/Developer/CommandLineTools/usr/bin/sourcekit-lsp'}
---}
+        hls = function()
+            require('lspconfig')['hls'].setup{
+                filetypes = { 'haskell', 'lhaskell', 'cabal' },
+            }
+        end,
 
-lsp.setup()
+        --sourcekit = function()
+        --    require('lspconfig').sourcekit.setup {
+        --        cmd = {'/Library/Developer/CommandLineTools/usr/bin/sourcekit-lsp'}
+        --    }
+        --end,
+
+        --omnisharp = function()
+        --    require('lspconfig').omnisharp.setup({
+        --        handlers = {
+        --            ["textDocument/definition"] = require('omnisharp_extended').handler,
+        --        }
+        --    })
+        --end,
+    },
+})
+
+local cmp = require("cmp")
+require('luasnip.loaders.from_vscode').lazy_load()
+
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+
+cmp.setup({
+    preselect = 'item',
+    completion = {
+        completeopt = 'menu, menuone,noinsert'
+    },
+    window = {
+        documentation = cmp.config.window.bordered(),
+    },
+    sources = {
+        { name = 'path' },
+        { name = 'nvim_lsp' },
+        { name = 'buffer', keyword_length = 3 },
+        { name = 'luasnip', keyword_length = 2 },
+    },
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end,
+    },
+    formatting = {
+        fields = { 'abbr', 'menu', 'kind' },
+        format = function(entry, item)
+            local n = entry.source.name
+            if n == 'nvim_lsp' then
+                item.menu = '[LSP]'
+            else
+                item.menu = string.format('[%s]', n)
+            end
+            return item
+        end,
+    },
+    mapping = {
+        -- Confirm comletion item
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+        -- Scroll docs window
+        ['<C-f>'] = cmp.mapping.scroll_docs(5),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-5),
+
+        -- Toggle completion menu
+        ['<C-e>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.abort()
+            else
+                cmp.complete()
+            end
+        end),
+
+        -- Tab complete
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            local col = vim.fn.col('.')-1
+            if cmp.visible() then
+                cmp.select_next_item({ behavior = 'select' })
+            elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+                fallback()
+            else
+                cmp.complete()
+            end
+        end, {'i', 's'}),
+
+        -- Previous item
+        ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+
+        -- Next snippet placeholder
+        ['<C-d>'] = cmp.mapping(function(fallback)
+            local luasnip = require('luasnip')
+            if luasnip.jumpable(1) then
+                luasnip.jump(1)
+            else
+                fallback()
+            end
+        end, {'i', 's'}),
+
+        ['<C-b>'] = cmp.mapping(function(fallback)
+            local luasnip = require('luasnip')
+            if luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, {'i', 's'}),
+
+        ['<C-Space>'] = cmp.mapping.complete(),
+    },
+})
+
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -6,46 +6,49 @@ return require('packer').startup(function(use)
 
   -- Colourschemes
   use 'Mordinel/vim-sunsurfer'
-  use 'f-person/auto-dark-mode.nvim'
+  use 'Mordinel/auto-dark-mode.nvim'
 
+  -- Git
   use 'tpope/vim-fugitive'
 
-  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-  use('nvim-treesitter/playground')
+  -- Treesitter
+  use { 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } }
+  use 'nvim-treesitter/playground'
 
+  -- LSP
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+
+  -- Autocomplete
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-nvim-lua'
+  use 'saadparwaiz1/cmp_luasnip'
+
+  -- Snippets
+  use 'L3MON4D3/LuaSnip'
+  use 'rafamadriz/friendly-snippets'
+
+  --use { 'VonHeikemen/lsp-zero.nvim', branch = 'v4.x' }
+
+  -- for TeX
   use {
-      'VonHeikemen/lsp-zero.nvim',
-      branch = 'v1.x',
-      requires = {
-          -- LSP Support
-          {'neovim/nvim-lspconfig'},             -- Required
-          {'williamboman/mason.nvim'},           -- Optional
-          {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-          -- Autocompletion
-          {'hrsh7th/nvim-cmp'},         -- Required
-          {'hrsh7th/cmp-nvim-lsp'},     -- Required
-          {'hrsh7th/cmp-buffer'},       -- Optional
-          {'hrsh7th/cmp-path'},         -- Optional
-          {'saadparwaiz1/cmp_luasnip'}, -- Optional
-          {'hrsh7th/cmp-nvim-lua'},     -- Optional
-
-          -- Snippets
-          {'L3MON4D3/LuaSnip'},             -- Required
-          {'rafamadriz/friendly-snippets'}, -- Optional
-      }
+    'lervag/vimtex',
+    tag = "v2.16",
+    opt = true,
+    config = function()
+        vim.g.vimtex_view_method = "zathura"
+    end,
+    ft = 'tex',
   }
 
   --use {
   --    'neoclide/coc.nvim',
   --    branch = 'release',
   --}
-
-  -- for TeX
-  -- use {
-  --   'lervag/vimtex',
-  --   tag = "v2.16",
-  -- }
 
   -- for viewing decompiled .NET IL code as C#
   -- use 'Hoffs/omnisharp-extended-lsp.nvim'
