644347c7d7781e21da6deac0b17493a4fb96fc1b
Mason <Mordinel@pm.me>
2025-07-30 10:01:37 +1200

    my-a config is-a working

--- nvim/init.lua
+++ nvim/init.lua
@@ -227,8 +227,13 @@ require("lazy").setup({
                     dependencies = { 'hrsh7th/cmp-nvim-lsp' },
                     opts = { },
                     config = function()
-                        vim.lsp.config('*', { })
-
+                        -- Add cmp_nvim_lsp capabilities settings to lspconfig
+                        local lspconfig_defaults = require('lspconfig').util.default_config
+                        lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+                            'force',
+                            lspconfig_defaults.capabilities,
+                            require('cmp_nvim_lsp').default_capabilities()
+                        )
                         vim.lsp.config('rust_analyzer', {
                             require('lspconfig').rust_analyzer.setup({
                                 cmd = { 'rust-analyzer' },
@@ -259,8 +264,12 @@ require("lazy").setup({
                             settings = {
                                 pylsp = {
                                     plugins = {
-                                        pycodestyle = {enabled = false},
-                                        pyflakes = {enabled = false},
+                                        pycodestyle = { enabled = false },
+                                        pyflakes = { enabled = false },
+                                        -- autopep8 = { enabled = false },
+                                        -- flake8 = { enabled = false },
+                                        -- pylint = { enabled = false },
+                                        -- yapf = { enabled = false },
                                     },
                                 },
                             },
@@ -306,65 +315,56 @@ require("lazy").setup({
                         --     }
                         -- })
 
-                        end,
-                        config = function()
-                            -- Add cmp_nvim_lsp capabilities settings to lspconfig
-                            local lspconfig_defaults = require('lspconfig').util.default_config
+                        vim.lsp.config('*', { })
+                    end,
+                    init = function()
+                        vim.opt.signcolumn = 'yes'
 
-                            lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-                                'force',
-                                lspconfig_defaults.capabilities,
-                                require('cmp_nvim_lsp').default_capabilities()
-                            )
-                        end,
-                        init = function()
-                            vim.opt.signcolumn = 'yes'
-
-                            -- Errors & Warnings
-                            vim.diagnostic.config({
-                                virtual_text = true,
-                                severity_sort = true,
-                                float = {
-                                    style  = 'minimal',
-                                    border = 'single',
-                                    header = '',
-                                    prefix = '',
+                        -- Errors & Warnings
+                        vim.diagnostic.config({
+                            virtual_text = true,
+                            severity_sort = true,
+                            float = {
+                                style  = 'minimal',
+                                border = 'single',
+                                header = '',
+                                prefix = '',
+                            },
+                            signs = {
+                                text = {
+                                    [vim.diagnostic.severity.ERROR] = '✘',
+                                    [vim.diagnostic.severity.WARN]  = '▲',
+                                    [vim.diagnostic.severity.HINT]  = '⚑',
+                                    [vim.diagnostic.severity.INFO]  = '»',
                                 },
-                                signs = {
-                                    text = {
-                                        [vim.diagnostic.severity.ERROR] = '✘',
-                                        [vim.diagnostic.severity.WARN]  = '▲',
-                                        [vim.diagnostic.severity.HINT]  = '⚑',
-                                        [vim.diagnostic.severity.INFO]  = '»',
-                                    },
-                                },
-                            })
-                            vim.api.nvim_create_autocmd('LspAttach', {
-                                group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-                                callback = function(ev)
-                                    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-                                    local opts = { buffer = ev.buf }
+                            },
+                        })
+                        vim.api.nvim_create_autocmd('LspAttach', {
+                            group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+                            callback = function(ev)
+                                vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+                                local opts = { buffer = ev.buf }
 
-                                    -- gotos
-                                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-                                    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-                                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                                -- gotos
+                                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
 
-                                    -- goto diagnostics
-                                    vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
-                                    vim.keymap.set('n', 'gnl', function() vim.diagnostic.jump({count= 1, float=true}) end, opts)
-                                    vim.keymap.set('n', 'gbl', function() vim.diagnostic.jump({count=-1, float=true}) end, opts)
+                                -- goto diagnostics
+                                vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
+                                vim.keymap.set('n', 'gnl', function() vim.diagnostic.jump({count= 1, float=true}) end, opts)
+                                vim.keymap.set('n', 'gbl', function() vim.diagnostic.jump({count=-1, float=true}) end, opts)
 
-                                    -- docs
-                                    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover({border='single'}) end, opts)
+                                -- docs
+                                vim.keymap.set('n', 'K', function() vim.lsp.buf.hover({border='single'}) end, opts)
 
-                                    -- code actions
-                                    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-                                    vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
-                                    vim.keymap.set('n', '<F3>', function() vim.lsp.buf.format({ async = true }) end, opts)
-                                end,
-                            })
-                        end,
+                                -- code actions
+                                vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+                                vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
+                                vim.keymap.set('n', '<F3>', function() vim.lsp.buf.format({ async = true }) end, opts)
+                            end,
+                        })
+                    end,
                     },
                 },
             },
